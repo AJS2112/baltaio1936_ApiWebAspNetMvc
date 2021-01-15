f0c7b2050a6a992c7ea10c541d96858e16348c45
Antonio Sucre <antonio.sucre@indikatore.com>
2021-01-15 10:18:32 -0300

    start

---
+++ DevStore.Data/Contexts/AppDataContext.cs
@@ -0,0 +1,18 @@
+﻿using DevStore.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevStore.Data.Contexts
+{
+    public class AppDataContext:DbContext
+    {
+        public AppDataContext() : base("AppCnnStr")
+        {}
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ DevStore.Domain/Repositories/IProductRepository.cs
@@ -0,0 +1,19 @@
+﻿using DevStore.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevStore.Domain.Repositories
+{
+    public interface IProductRepository : IDisposable
+    {
+        List<Product> Get();
+        Product Get(int id);
+        void Create(Product product);
+        void Update(Product product);
+        void Delete(int id);
+
+    }
+}
